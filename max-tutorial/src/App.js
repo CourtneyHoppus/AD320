f26e8aa28fdb2ce4be0691a303fc99b35f8ad60e
CourtneyHoppus <74206103+CourtneyHoppus@users.noreply.github.com>
2021-01-24 12:00:59 -0800

    continuing tutorial

--- max-tutorial/src/App.js
+++ max-tutorial/src/App.js
@@ -3,12 +3,22 @@ import './App.css';
 import Person from './Person/Person';
 
 class App extends Component { 
+  state = {
+    persons: [
+      { name: 'Max', age: 28 },
+      { name: 'Me', age: 41 },
+      { name: 'Brad', age: 38 }
+    ]
+  }
   render() {
     return (
       <div className="App">
         <h1>Test React App</h1>
         <p>This is really going on.</p>
-        <Person />
+        <button>Switch Name</button>
+        <Person name={this.state.persons[0].name} age={this.state.persons[0].age} />
+        <Person name={this.state.persons[1].name} age={this.state.persons[1].age} />
+        <Person name={this.state.persons[2].name} age={this.state.persons[2].age} />
       </div>
     );
   }
